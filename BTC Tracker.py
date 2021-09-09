e48acf3e0d04b990863dff64c41bf761c129b0fd
Prabeen-Raj <67351709+Prabeen-Raj@users.noreply.github.com>
2021-09-09 17:45:12 +0530

    Add files via upload

---
+++ BTC Tracker.py
@@ -0,0 +1,20 @@
+import requests
+import serial
+import time
+from bs4 import BeautifulSoup
+
+s = serial.Serial('COM2', baudrate=9600)
+time.sleep(2)
+i = 0
+
+if s.isOpen():
+  while (True):
+      while i < 1:
+          url = 'https://www.google.com/search?&q=bitcoin price in america'
+          req = requests.get(url)
+          scrap = BeautifulSoup(req.text, 'html.parser')
+          bitcoin_price = scrap.find("div", class_="BNeawe iBp4i AP7Wnd").text
+          s.write(bitcoin_price.encode())
+         
+  
+  
