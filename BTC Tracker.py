29fd370acb7bbcf1bc76d4aa84205d7f71574e4d
Prabeen-Raj <67351709+Prabeen-Raj@users.noreply.github.com>
2021-09-10 12:09:19 +0530

    Update BTC Tracker.py

--- BTC Tracker.py
+++ BTC Tracker.py
@@ -3,18 +3,19 @@ import serial
 import time
 from bs4 import BeautifulSoup
 
-s = serial.Serial('COM2', baudrate=9600)
+s = serial.Serial('COM3', baudrate=9600)
 time.sleep(2)
 i = 0
 
 if s.isOpen():
   while (True):
       while i < 1:
-          url = 'https://www.google.com/search?&q=bitcoin price in america'
+          url = 'https://www.google.com/search?&q=bitcoin price in india'
           req = requests.get(url)
           scrap = BeautifulSoup(req.text, 'html.parser')
           bitcoin_price = scrap.find("div", class_="BNeawe iBp4i AP7Wnd").text
+          print(bitcoin_price)
           s.write(bitcoin_price.encode())
-         
+
   
   
